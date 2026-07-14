203d0879e0978cbfb3536fcdcbf3e5f45296dcd7
江原 圭 <matador.sangria@gmail.com>
2026-07-14 17:19:24 +0900

    standalone対応

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout() {
     <AppProvider>
       <Head>
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="冷蔵庫" />
       </Head>
       <AppStack />
     </AppProvider>
